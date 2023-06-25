91e7db216465a3c8053276a786844249ca1fd7d4
13virat <viratgupta1314@gmail.com>
2023-06-25 11:42:06 +0530

    nn

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,10 +1,9 @@
-
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .serializer import ServerSerializer
 
 from .models import Server
+from .serializer import ServerSerializer
 
 
 class ServiceListViewSet(viewsets.ViewSet):
@@ -12,9 +11,11 @@ class ServiceListViewSet(viewsets.ViewSet):
 
     def list(self, request):
         category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
         if category:
             self.queryset = self.queryset.filter(category__name=category)
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
-
