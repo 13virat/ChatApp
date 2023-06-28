ee2a9a7d51698d73d76792aedebd62bf81c47a1d
13virat <viratgupta1314@gmail.com>
2023-06-28 20:19:05 +0530

    bsb

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
@@ -15,6 +16,7 @@ class ServiceListViewSet(viewsets.ViewSet):
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if by_user and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -24,17 +26,18 @@ class ServiceListViewSet(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
+
         if qty:
             self.queryset = self.queryset[: int(qty)]
         if by_serverid:
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
-                    raise ValidationError(
-                        detail=f"Server eith id {by_serverid} not found"
-                    )
+                    raise ValidationError(detail=f"Server eith id {by_serverid} not found")
             except ValueError:
                 raise ValidationError(detail="Server value error")
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True,context={"num_members":with_num_members})
         return Response(serializer.data)
