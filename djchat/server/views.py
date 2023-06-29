a3736337a857bbf4f9b062f4a6c3df440392cc4d
13virat <viratgupta1314@gmail.com>
2023-06-29 12:42:55 +0530

    quick code + docstring

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -11,32 +11,71 @@ class ServiceListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
     def list(self, request):
+        """Fetches a list of servers based on the provided query parameters.
+
+        Args:
+            request (Request): The HTTP request object containing the query parameters.
+
+        Returns:
+            Response: The HTTP response containing the serialized server data.
+
+        Raises:
+            AuthenticationFailed: If the 'by_user' parameter is set to true but the user is not authenticated.
+            ValidationError: If there is an error with the query parameter values.
+
+        Query Parameters:
+            - category (str): Optional. Filters the servers by the specified category name.
+            - qty (int): Optional. Limits the number of servers to be returned.
+            - by_user (bool): Optional. If set to true, filters the servers by the currently authenticated user.
+            - by_serverid (str): Optional. Filters the servers by the specified server ID.
+            - with_num_members (bool): Optional. If set to true, includes the number of members for each server.
+
+        Note:
+            - The 'by_user' parameter requires the user to be authenticated. Otherwise, an AuthenticationFailed exception is raised.
+            - The 'by_serverid' parameter should be a valid server ID. If the server with the specified ID is not found, a ValidationError is raised.
+            - The 'with_num_members' parameter, when set to true, adds an additional 'num_members' field to each serialized server object.
+
+        """
+
+        # Extract query parameters from the request
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
         with_num_members = request.query_params.get("with_num_members") == "true"
 
+        # Check if authentication is required and if the user is authenticated
         if by_user and not request.user.is_authenticated:
             raise AuthenticationFailed()
 
+        # Filter queryset based on category, if provided
         if category:
             self.queryset = self.queryset.filter(category__name=category)
+
+        # Filter queryset based on the authenticated user, if requested
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+
+        # Annotate queryset with the number of members, if requested
         if with_num_members:
             self.queryset = self.queryset.annotate(num_members=Count("member"))
 
+        # Limit the queryset to a specific quantity, if provided
         if qty:
             self.queryset = self.queryset[: int(qty)]
+
+        # Filter queryset based on server ID, if provided
         if by_serverid:
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
-                    raise ValidationError(detail=f"Server eith id {by_serverid} not found")
+                    raise ValidationError(detail=f"Server with id {by_serverid} not found")
             except ValueError:
                 raise ValidationError(detail="Server value error")
 
+        # Serialize the queryset using the ServerSerializer
         serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
+
+        # Return the serialized data as a response
         return Response(serializer.data)
